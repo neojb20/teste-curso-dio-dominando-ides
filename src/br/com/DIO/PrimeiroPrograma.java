899e0ab5bdeb12e215b6acb4255a7e51ad57da7c
Nilson <neojb20@gmail.com>
2021-07-25 23:19:32 -0300

    First Commit

---
+++ src/br/com/DIO/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.DIO;
+
+import br.com.DIO.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+
+//        int a = 5;
+//        int b = 3;
+//
+//        System.out.println("Hello Wold"  + (a+b));
+    }
+
+}
---
+++ src/br/com/DIO/model/Gato.java
@@ -0,0 +1,8 @@
+package br.com.DIO.model;
+
+public class Gato {
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+}
